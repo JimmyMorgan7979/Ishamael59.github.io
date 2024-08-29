9faa291bc1cbe96d137f15ed974882cbae021410
[JimmyMorgan7979] <[jimmymorgan7979@gmail.com]>
2024-08-29 14:35:05 -0400

    Added Page for Nashville Number Converter

---
+++ scripts/nashvilleNumber.js
@@ -0,0 +1,136 @@
+function keyChange(){
+    var newKey = document.getElementById('keyValue').value;
+
+    if(newKey.toLowerCase() == 'a'){
+        document.getElementById('key').textContent= "A"
+        document.getElementById('1').textContent= "A"
+        document.getElementById('2').textContent= "Bm"
+        document.getElementById('3').textContent= "C#m"
+        document.getElementById('4').textContent= "D"
+        document.getElementById('5').textContent= "E"
+        document.getElementById('6').textContent= "F#m"
+        document.getElementById('7').textContent= "G#dim"
+    }
+   
+    if(newKey.toLowerCase() == 'bb'){
+        document.getElementById('key').textContent= "Bb"
+        document.getElementById('1').textContent= "Bb"
+        document.getElementById('2').textContent= "Cm"
+        document.getElementById('3').textContent= "Dm"
+        document.getElementById('4').textContent= "Eb"
+        document.getElementById('5').textContent= "F"
+        document.getElementById('6').textContent= "Gm"
+        document.getElementById('7').textContent= "Adim"
+    }
+    
+    if(newKey.toLowerCase() == 'b'){
+        document.getElementById('key').textContent= "B"
+        document.getElementById('1').textContent= "B"
+        document.getElementById('2').textContent= "C#m"
+        document.getElementById('3').textContent= "D#m"
+        document.getElementById('4').textContent= "E"
+        document.getElementById('5').textContent= "F#"
+        document.getElementById('6').textContent= "G#m"
+        document.getElementById('7').textContent= "A#dim"
+    }
+    
+    if(newKey.toLowerCase() == 'c'){
+        document.getElementById('key').textContent= "C"
+        document.getElementById('1').textContent= "C"
+        document.getElementById('2').textContent= "Dm"
+        document.getElementById('3').textContent= "Em"
+        document.getElementById('4').textContent= "F"
+        document.getElementById('5').textContent= "G"
+        document.getElementById('6').textContent= "Am"
+        document.getElementById('7').textContent= "Bdim"
+    }
+    
+    if(newKey.toLowerCase() == 'db'){
+        document.getElementById('key').textContent= "Db"
+        document.getElementById('1').textContent= "Db"
+        document.getElementById('2').textContent= "Ebm"
+        document.getElementById('3').textContent= "Fm"
+        document.getElementById('4').textContent= "Gb"
+        document.getElementById('5').textContent= "Ab"
+        document.getElementById('6').textContent= "Bbm"
+        document.getElementById('7').textContent= "Cdim"
+    }
+   
+    if(newKey.toLowerCase() == 'd'){
+        document.getElementById('key').textContent= "D"
+        document.getElementById('1').textContent= "D"
+        document.getElementById('2').textContent= "Em"
+        document.getElementById('3').textContent= "F#m"
+        document.getElementById('4').textContent= "G"
+        document.getElementById('5').textContent= "A"
+        document.getElementById('6').textContent= "Bm"
+        document.getElementById('7').textContent= "C#dim"
+    }
+
+    if(newKey.toLowerCase() == 'eb'){
+        document.getElementById('key').textContent= "Eb"
+        document.getElementById('1').textContent= "Eb"
+        document.getElementById('2').textContent= "Fm"
+        document.getElementById('3').textContent= "Gm"
+        document.getElementById('4').textContent= "Ab"
+        document.getElementById('5').textContent= "Bb"
+        document.getElementById('6').textContent= "Cm"
+        document.getElementById('7').textContent= "Ddim"
+    }
+    
+    if(newKey.toLowerCase() == 'e'){
+        document.getElementById('key').textContent= "E"
+        document.getElementById('1').textContent= "E"
+        document.getElementById('2').textContent= "F#m"
+        document.getElementById('3').textContent= "G#m"
+        document.getElementById('4').textContent= "A"
+        document.getElementById('5').textContent= "B"
+        document.getElementById('6').textContent= "C#m"
+        document.getElementById('7').textContent= "D#dim"
+    }
+    
+    if(newKey.toLowerCase() == 'f'){
+        document.getElementById('key').textContent= "F"
+        document.getElementById('1').textContent= "F"
+        document.getElementById('2').textContent= "Gm"
+        document.getElementById('3').textContent= "Am"
+        document.getElementById('4').textContent= "Bb"
+        document.getElementById('5').textContent= "C"
+        document.getElementById('6').textContent= "Dm"
+        document.getElementById('7').textContent= "Edim"
+    }
+    
+    if(newKey.toLowerCase() == 'gb'){
+        document.getElementById('key').textContent= "Gb"
+        document.getElementById('1').textContent= "Gb"
+        document.getElementById('2').textContent= "Abm"
+        document.getElementById('3').textContent= "Bbm"
+        document.getElementById('4').textContent= "Cb"
+        document.getElementById('5').textContent= "Db"
+        document.getElementById('6').textContent= "Ebm"
+        document.getElementById('7').textContent= "Fdim"
+    }
+    
+    if(newKey.toLowerCase() == 'g'){
+        document.getElementById('key').textContent= "G"
+        document.getElementById('1').textContent= "G"
+        document.getElementById('2').textContent= "Am"
+        document.getElementById('3').textContent= "Bm"
+        document.getElementById('4').textContent= "C"
+        document.getElementById('5').textContent= "D"
+        document.getElementById('6').textContent= "Em"
+        document.getElementById('7').textContent= "F#dim"
+    }
+    
+    if(newKey.toLowerCase() == 'ab'){
+        document.getElementById('key').textContent= "Ab"
+        document.getElementById('1').textContent= "Ab"
+        document.getElementById('2').textContent= "Bbm"
+        document.getElementById('3').textContent= "Cm"
+        document.getElementById('4').textContent= "Db"
+        document.getElementById('5').textContent= "Eb"
+        document.getElementById('6').textContent= "Fm"
+        document.getElementById('7').textContent= "Gdim"
+    }
+    
+  }
